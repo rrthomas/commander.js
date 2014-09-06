d4f19f2eee93100c4f74141aaeadbf81b76acfdd
Reuben Thomas <rrt@sc3d.org>
2014-09-06 21:34:41 +0100

    Fix a typo in index.js
    Extra arguments to a command's `action` function are "supplied" to it, not "applied" to it.

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ function Command(name) {
  *
  * The `.action()` callback is invoked when the
  * command `name` is specified via __ARGV__,
- * and the remaining arguments are applied to the
+ * and the remaining arguments are supplied to the
  * function for access.
  *
  * When the `name` is "*" an un-matched command
